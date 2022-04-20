229c80bbcdc523b62a958cf15e24cf411e63e492
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-20 11:20:51 +0200

    read account's ether balance

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { useEthers } from '@usedapp/core';
+import { useEtherBalance, useEthers } from '@usedapp/core';
 import { shortenAddress } from '../utils/helpers';
 import { useBid } from '../utils/hooks';
+import { formatEther } from 'ethers/lib/utils';
 
 const Header = () => {
   const { activateBrowserWallet, deactivate, account } = useEthers();
   const { auctionInterface } = useBid();
+  const accountBalance = useEtherBalance(account);
   console.log('in Header', auctionInterface);
   return (
     <div>
-      <h2>
-        {!account ? "Please connect your wallet." : `Connected Wallet: ${shortenAddress(account)}`}
-      </h2>
+      {!account ? (
+        <h2>
+          Please connect your wallet.
+        </h2>
+      )
+        : (
+          <h2>
+            {`Connected Wallet: ${shortenAddress(account)}`}
+            <br />
+            {`Your Balance: ${formatEther(accountBalance)} Eth`}
+          </h2>
+        )
+      }
       {!account ? (
         <button className="connect" onClick={() => activateBrowserWallet()}>
           Connect
